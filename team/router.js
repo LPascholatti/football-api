db9c86483c0ee87067b99fd2654c0b4715f4f57f
LPascholatti <lucaspascholatti@gmail.com>
2019-09-04 16:50:34 +0200

    finished setting up the API

---
+++ team/router.js
@@ -0,0 +1,25 @@
+const { Router } = require('express')
+const Team = require('./model.js')
+const router = new Router()
+
+router.get('/team', (req, res, next) => 
+Team.findAll()
+.then(teamList => res.json(teamList))
+.catch(next)
+);
+
+router.get('/team/:id', (req, res, next) =>
+Team.findByPk(req.params.id)
+.then(id => res.json(id))
+.catch(next)
+);
+
+router.post('/team', (req, res, next) => 
+Team.create(req.body)
+.then(newTeam => res.json(newTeam))
+.catch(next)
+);
+
+
+module.exports = router;
+
